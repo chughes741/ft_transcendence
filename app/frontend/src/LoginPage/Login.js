bc275b115df1fa463efa7343c01e718a1ee4b4ef
Jean-rene Vigneault <jvigneau@42quebec.com>
2023-02-27 17:06:49 -0500

    added the login page in the appfrontend

---
+++ app/frontend/src/LoginPage/Login.js
@@ -0,0 +1,15 @@
+export default function Login() {
+  return (
+    <div>
+      <div class="login-info-container">
+        <input class="login-field" type="text" placeholder="put your fucking nick right here" />
+      </ div>
+      <div class="login-info-container">
+        <input class="login-field" type="password" placeholder="put your fucking pass here" />
+      </div>
+      <div class="login-info-container">
+        <button class="bouton main" type="submit">Submit bitches</button>
+      </div>
+    </div>
+  )
+}
