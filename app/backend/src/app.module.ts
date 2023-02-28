af936a00e1015a93bcb6f32f00c20451050b2873
Cole Hughes <coltenhughes@gmail.com>
2023-02-28 16:35:18 -0500

    Added static serve to backend
    Updated the module:imports to use ServeStaticModule

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
-import { LoginService } from './login/login.service';
-import { LoginController } from './login/login.controller';
+import { LoginService } from "./login/login.service";
+import { LoginController } from "./login/login.controller";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "..", "frontend", "build"),
+    }),
+  ],
   controllers: [AppController, LoginController],
   providers: [AppService, LoginService],
 })
