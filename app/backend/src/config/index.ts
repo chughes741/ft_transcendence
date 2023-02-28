1ebfbafdc24f94ad17db9b1f8e3f2afcf5971ce2
Cole Hughes <coltenhughes@gmail.com>
2023-02-28 13:36:07 -0500

    Added dotenv for loading .env for the backend

---
+++ app/backend/src/config/index.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config();
+
+export default {
+  // Access port
+  port: parseInt(process.env.PORT, 10),
+
+  // PostgreSQL database name
+  databasename: process.env.PGDATABASE,
+
+  // PostgreSQL username
+  databaseuser: process.env.PGUSER,
+
+  // PostgreSQL password
+  databasepw: process.env.PGPASSWORD,
+};
