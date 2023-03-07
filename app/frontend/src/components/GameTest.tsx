a51686565d6dfe8e26559405c6d4e0a6aa065568
jgoad <jon@weareopensunday.com>
2023-03-07 15:15:03 -0500

    Added react-three/fiber
    - Basic render objext on gametest page

---
+++ app/frontend/src/components/GameTest.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+import styled from 'styled-components';
+
+// const FrameCounter = () => {
+
+
+// 	return (
+// 		<mesh>
+// 			<boxGeometry />
+
+// 		</mesh>
+// 	)
+// }
+
+const MyMesh = () => {
+	// useFrame(({ clock }) => {
+	// 	const a = clock.getElapsedTime()
+	// 	console.log(a)
+	// })
+	return (
+		<mesh>
+			<sphereGeometry />
+			<meshPhongMaterial color="hotpink" transparent />
+		</mesh>
+	)
+}
+
+export default function Game() {
+
+
+
+
+
+
+
+
+
+
+
+
+	return (
+		<GameWindow>
+			<Canvas>
+				<MyMesh />
+				<ambientLight args={[0xff0000]} intensity={0.1} />
+				<directionalLight position={[0, 5, 3]} intensity={0.5} />
+			</Canvas>
+		</GameWindow>
+	);
+}
+
+
+const GameWindow = styled.div`
+	height: 80vh;
+	width: 30vw;
+	color: #fff;
+`;
+
