3abb3ff164c7825aaff5435d606726ce8469b2db
Jean-rene Vigneault <jvigneau@42quebec.com>
2023-03-08 11:51:07 -0500

    the fucking buttons in the popup now work

--- app/frontend/src/popups/PopUp.tsx
+++ app/frontend/src/popups/PopUp.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Button from '../components/Button';
 import "./PopUp.tsx.css";
 import { useNavigate } from 'react-router-dom';
 import Icon from "../components/Icon";
@@ -6,9 +7,12 @@ import { FiMenu } from 'react-icons/fi';
 
 const IconsContainer = styled.div`
   display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
   margin: 1rem 0 0 1rem;
-  width: 80%;
-  height: 100vh;
+  width: 5vw;
+  height: 8vh;
 `;
 
 export default function PopUp({ id }) {
@@ -16,14 +20,14 @@ export default function PopUp({ id }) {
   const navigate = useNavigate();
   const bngColor = "linear-gradient(to right, #14163c 0%, #03217b 79%)";
 
-    function clickPopup()   {
-      if (document.querySelector("#popup").classList.contains('active')) {
-        document.querySelector("#popup-button-game").classList.remove('active');
-        document.querySelector("#popup-button-login").classList.remove('active');
-        document.querySelector("#popup-button-chat").classList.remove('active');
-        document.querySelector("#popup").classList.remove('active');
-      }
-      else {
+  function clickPopup()   {
+    if (document.querySelector("#popup").classList.contains('active')) {
+      document.querySelector("#popup-button-game").classList.remove('active');
+      document.querySelector("#popup-button-login").classList.remove('active');
+      document.querySelector("#popup-button-chat").classList.remove('active');
+      document.querySelector("#popup").classList.remove('active');
+    }
+    else {
         document.querySelector("#popup").classList.add('active');
         document.querySelector("#popup-button-game").classList.add('active');
         document.querySelector("#popup-button-login").classList.add('active');
@@ -51,13 +55,13 @@ export default function PopUp({ id }) {
         </Icon>
       </IconsContainer>
       <div className="popup-container" id={id}>
-        <div className="button-container" >
+        <div className="button-container">
          <button className="button-popup" id="popup-button-game" onClick={navGame}>Game</button>
         </div>
-        <div className="button-container" >
+        <div className="button-container">
          <button className="button-popup" id="popup-button-chat" onClick={navChat}>Chat</button>
         </div>
-        <div className="button-container" >
+        <div className="button-container">
          <button className="button-popup" id="popup-button-login" onClick={navLogin}>Login</button>
         </div>
       </div>
