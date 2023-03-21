7d5712e3f88da09e38b26d4195c4e5a21eb4ec22
Cole <95713039+chughes741@users.noreply.github.com>
2023-03-21 16:30:19 -0400

    Fixed backend/chat compile error

--- app/backend/src/chat/chat.module.ts
+++ app/backend/src/chat/chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { ChatService } from "./chat.service";
 import { ChatGateway } from "./chat.gateway";
 import { PrismaModule } from "../prisma/prisma.module";
+import { UserConnectionsService } from "src/user-connections.service";
 
 @Module({
   imports: [PrismaModule],
-  providers: [ChatGateway, ChatService]
+  providers: [ChatGateway, ChatService, UserConnectionsService]
 })
 export class ChatModule {}
