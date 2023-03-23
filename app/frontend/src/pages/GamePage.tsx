4da19e185b228f57e28295e2289d91c5bbb16dc9
Jean-rene Vigneault <jvigneau@42quebec.com>
2023-03-23 15:31:54 -0400

    gamePage frontend start

--- app/frontend/src/pages/GamePage.tsx
+++ app/frontend/src/pages/GamePage.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Helmet} from "react-helmet";
 import SideBar from "src/components/SideBar";
 import GameSideBar from "src/components/GameSideBar";
 import Game from "src/pages/game/GameWindow";
 import "src/pages/GamePage.tsx.css";
 import {useState} from "react";
+import
 
 export default function GamePage() {
+
+  const [buttonIsActive, setButtonActive] = useState(false);
+  const [chatIsActive, setChatActive] = useState(false);
+  const [chatButtonIsActive, setChatButtonActive] = useState(true);
+
+  function ClickJoinLobby() {
+    console.log("clicker join lobby");
+    setButtonActive(true);
+  }
+
+  function ClickCancel() {
+    console.log("clicked cancel");
+    setButtonActive(false);
+    setChatActive(false);
+  }
+
+  function ClickChat() {
+    setChatActive(!chatIsActive);
+    setChatButtonActive(!chatButtonIsActive);
+  }
+
   return (
     <>
       <Helmet>
         <title>
-          Transcendence | Game <i>Hello</i>
+          Transcendence | Game
         </title>
       </Helmet>
       <div className="game-page">
@@ -21,15 +43,20 @@ export default function GamePage() {
             <Game/>
           </div>
           <div className="buttons-container">
-            <div className="button-wrapper">
-              <button className="join-lobby">Joino auno loobyo</button>
+            <div className={`button-wrapper ${buttonIsActive ? "button-inactive" : ""}`}>
+              <button className="join-lobby" onClick={ClickJoinLobby}>Joino auno loobyo</button>
             </div>
-            <div className="button-wrapper">
-              <button className="cancel">Cancellllio</button>
+            <div className={`button-wrapper ${buttonIsActive ? "" : "button-inactive"}`}>
+              <button className="cancel" onClick={ClickCancel}>Cancellllio</button>
             </div>
+            <div className={`button-wrapper ${buttonIsActive ? "" : "button-inactive"}`}>
+              <button onClick={ClickChat} className={`chat-button ${chatButtonIsActive ? "" : "close-active"}`}>Open chat</button>
+              <button onClick={ClickChat} className={`chat-button ${chatButtonIsActive ? "close-active" : ""}`}>Close chat</button>
+            </div>
+
           </div>
-          <div className="game-side-bar-area">
-            <GameSideBar/>
+          <div className={`game-side-bar-area ${chatIsActive ? "" : "inactive"}`}>
+            <GameSideBar />
           </div>
         </div>
       </div>
