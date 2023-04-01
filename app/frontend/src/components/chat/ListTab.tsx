96bb3cf970c2c79ad00b772c6a1099f93af7fed5
Andre-Paul Grenon <agrenon@gmail.com>
2023-04-01 11:49:01 -0400

    New Component ListTabulation

--- app/frontend/src/components/chat/ListTab.tsx
+++ app/frontend/src/components/chat/ListTab.tsx
@@ -1,60 +1,75 @@
 
 import './ListTab.tsx.css'
-import { Box } from "@mui/material";
+import { Box, Typography, AppBar } from "@mui/material";
+import { List, ListItem, ListItemIcon, ListItemText, ListItemButton } from '@mui/material';
 import { MouseEvent, useState } from 'react';
-import {UserType} from '../User';
+import { UserType } from '../User';
+import { AccountCircle } from '@material-ui/icons';
 
-interface Props{
-    users : UserType[];
+
+interface Props {
+    users: UserType[];
     heading: string;
-    onSelectItem: (user : UserType) => void
-}   
+    onSelectItem: (user: UserType) => void
+}
 
-function ListTabulation( {users , heading, onSelectItem} : Props) {
+function ListTabulation({ users, heading, onSelectItem }: Props) {
 
-    const items = [
-        'Mario',
-        'Paris',
-        'Anne_putride',
-    ];
-    
     //State hook: means that this function will have variable that will change over time.
     //if the change of our function is updated, React will automatically update the DOM for us
-    const [SelectedIndex, setSelectedIndex] = useState(-1);// Variable (SelectedIndex) and updater function
+    //const [SelectedIndex, setSelectedIndex] = useState(-1);// Variable (SelectedIndex) and updater function
     // const [name, setName ] = useState(''); this could be use to modify also the state of the name
+
+    const [SelectedIndex, setSelectedIndex] = useState(-1);
 
     return (
         <>
-            <div style={{
+            <Box style={{
+                height : '100%',
                 position: 'fixed',
-                top: '50%',
                 right: '0',
-                transform: 'translateY(-50%)'
             }}>
-                <Box sx={{height: '65px'}}></Box>
-                <Box border={2} borderColor="black" p={2} display="inline-block">
-                    
-                    <div className="list-title">{heading}</div>
-                    
-                    {users.length === 0 && <div>No one in chat </div> } 
-                    
-                    <ul className="list-tab">
-                        {users.map( (users, index) => (
-                            <li 
-                            className = {SelectedIndex === index ? 'list-group-active' : 'list-group-inactive' }
-                            key = {users.uuid} //don't forget to add user.id unique key
-                            onClick = { () => {
-                                setSelectedIndex(index);
-                                onSelectItem(users)
-                            }}
-                            > {users.nick}  
-                            </li> 
+                <Box sx={{ height: '18px' }}></Box>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+
+                    <Box className="list-title">{heading}</Box>
+
+                    {users.length === 0 && <Box>No one in chat </Box>}
+
+                    <List>
+                        {users.map((users, index) => (
+                            <ListItemButton 
+                                selected={SelectedIndex === index ? true : false}
+                                key={users.uuid} //don't forget to add user.id unique key
+                                onClick={() => {
+                                    setSelectedIndex(index)
+                                    onSelectItem(users)
+                                }}
+                            >
+                                <ListItemIcon>
+                                    <img src="/home/agrenon/projets/ft_transcendence/app/src/42Logo.jpg" />
+                                </ListItemIcon>
+                                <ListItemText primary={users.nick} secondary = {users.email} />
+                            </ListItemButton>
                         ))}
-                    </ul>
+                    </List>
                 </Box>
-            </div>
+            </Box>
         </>
     )
 }
+
+/* //To Put instead of list item
+<List className="list-tab">
+                        {users.map((users, index) => (
+<li 
+className={SelectedIndex === index ? 'list-group-active' : 'list-group-inactive'}
+key={users.uuid} //don't forget to add user.id unique key
+onClick={() => {
+    setSelectedIndex(index);
+    onSelectItem(users)
+}}
+> {users.nick}
+</li>*/
 
 export default ListTabulation;
