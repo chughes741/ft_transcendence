a397d7d1f4edb5bf106a31e9ba2f65502ca57d8c
Andre-Paul Grenon <agrenon@gmail.com>
2023-04-02 21:37:18 -0400

    new Prismareference

--- app/backend/src/userlist/userlist.module.ts
+++ app/backend/src/userlist/userlist.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserlistService } from './userlist.service';
 import { UserlistGateway } from './userlist.gateway';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from "../prisma/prisma.service";
 
 
 @Module({
--- app/backend/src/userlist/userlist.service.ts
+++ app/backend/src/userlist/userlist.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from "../prisma/prisma.service";
 
 
 
