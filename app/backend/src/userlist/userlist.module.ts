7c728d32db14dd1634420432ecd936a512754709
Andre-Paul Grenon <agrenon@gmail.com>
2023-04-02 21:59:50 -0400

    Import :Prisma Service

--- app/backend/src/userlist/userlist.module.ts
+++ app/backend/src/userlist/userlist.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserlistService } from './userlist.service';
 import { UserlistGateway } from './userlist.gateway';
 import { PrismaService } from "../prisma/prisma.service";
+import { Prisma } from '@prisma/client';
 
 
 @Module({
-  
+  imports : [PrismaService],
   providers: [UserlistService, UserlistGateway, PrismaService]
 })
 export class UserlistModule {}
