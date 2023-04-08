26dd6ed1ea918481ad4f52d391f1c71433f6d052
Your Name <you@example.com>
2023-04-08 15:41:17 -0400

    WIP do not touch

--- app/frontend/src/pages/chat/components/Userlist.viewmodel.tsx
+++ app/frontend/src/pages/chat/components/Userlist.viewmodel.tsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { socket } from "../../../contexts/WebSocketContext";
 import {useChatViewModelContext} from "../contexts/ChatViewModelContext";
+import {UserListItem} from "./Userlist.model";
 
 export function getUserList() {
-    const [users, setUsers] = useState<myUsers[]>([]);
+    const [users, setUsers] = useState<UserListItem[]>([]);
     const {currentRoomName} = useChatViewModelContext();
     // Send "listUsers" event to server to get the user list
     useEffect(() => {
         socket.emit("listUsers", {currentRoomName});
-        socket.on("userList", (users: myUsers[]) => {
+        socket.on("userList", (users: UserListItem[]) => {
             console.log("New Use effect call to set UserList");
             setUsers(users);
         });
@@ -18,7 +19,7 @@ export function getUserList() {
     }, [socket,users]);
 }
 
-export const handleSelectItem = (user: myUsers) => {
+export const handleSelectItem = (user:UserListItem) => {
     console.log(user);
 };
 
