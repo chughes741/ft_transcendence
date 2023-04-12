3ae116be78acaeb415b6f8c39f1ad47d11a48966
Cole Hughes <coltenhughes@gmail.com>
2023-04-12 10:14:12 -0400

    Added basic modal for settings

---
+++ app/frontend/src/profile/settings/settings.view.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Box, Button, Modal, Typography } from "@mui/material";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4
+};
+
+/**
+ * SettingsView
+ *
+ * @returns {JSX.Element}
+ */
+export default function SettingsView() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button onClick={handleOpen}>Open Modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+          >
+            Text in a modal
+          </Typography>
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2 }}
+          >
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </>
+  );
+}
