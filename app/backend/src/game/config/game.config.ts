d9153aedc9777c35078f5adb658c4438680819db
jgoad <jon@weareopensunday.com>
2023-04-15 15:03:09 -0400

    Fixing the paddle collision logic

--- app/backend/src/game/config/game.config.ts
+++ app/backend/src/game/config/game.config.ts
@@ -1,19 +1,5 @@
 import { Vec2 } from "../vector";
 
-export class PaddleConfig {
-  static readonly height: number = 0.5;
-  static readonly width: number = 0.5;
-  static readonly depth: number = 0.5;
-  static readonly borderOffset: number = 0.1;
-}
-
-export class BallConfig {
-  static readonly initialSpeed: number = 0.75;
-  static readonly maxSpeed: number = 2;
-  static readonly maxServeAngle: number = 120;
-  static readonly radius: number = 0.1;
-}
-
 export class GameConfig {
   static readonly playAreaWidth: number = 6;
   static readonly playAreaHeight: number = 4;
@@ -24,6 +10,25 @@ export class GameConfig {
   static readonly botRight: Vec2 = {x: GameConfig.playAreaWidth / 2, y: -(GameConfig.playAreaHeight / 2)};
 
 }
+export class PaddleConfig {
+  static readonly height: number = 0.5;
+  static readonly width: number = 0.5;
+  static readonly depth: number = 0.5;
+  static readonly borderOffset: number = 0.1;
+  static readonly paddleLeftTop: Vec2 = {x: -(GameConfig.playAreaWidth / 2) + PaddleConfig.borderOffset, y: GameConfig.playAreaHeight / 2};
+  static readonly paddleLeftBot: Vec2 = {x: -(GameConfig.playAreaWidth / 2) + PaddleConfig.borderOffset, y: -(GameConfig.playAreaHeight / 2)};
+  static readonly paddleRightTop: Vec2 = {x: (GameConfig.playAreaWidth / 2) - PaddleConfig.borderOffset, y: GameConfig.playAreaHeight / 2};
+  static readonly paddleRightBot: Vec2 = {x: (GameConfig.playAreaWidth / 2) - PaddleConfig.borderOffset, y: -(GameConfig.playAreaHeight / 2)};
+}
+
+export class BallConfig {
+  static readonly initialSpeed: number = 0.75;
+  static readonly maxSpeed: number = 2;
+  static readonly maxServeAngle: number = 120;
+  static readonly radius: number = 0.1;
+}
+
+
 
 //Colours
 export class GameColours {
--- app/backend/src/game/game.logic.ts
+++ app/backend/src/game/game.logic.ts
@@ -32,32 +32,32 @@ export class GameLogic {
 
   //Initialize new game
   initNewGame(players: string[]): GameData {
-    const gameData: GameData = new GameData();
+    const gamestate: GameData = new GameData();
 
     //Setup general game properties
-    gameData.is_new_round = true;
-    gameData.bounds.width = GameConfig.playAreaWidth;
-    gameData.bounds.height = GameConfig.playAreaHeight;
-    gameData.players_ready = 0;
-    gameData.players = [];
-    gameData.players[0] = players[0];
-    gameData.players[1] = players[1];
+    gamestate.is_new_round = true;
+    gamestate.bounds.width = GameConfig.playAreaWidth;
+    gamestate.bounds.height = GameConfig.playAreaHeight;
+    gamestate.players_ready = 0;
+    gamestate.players = [];
+    gamestate.players[0] = players[0];
+    gamestate.players[1] = players[1];
 
     //Randomize serve side for initial serve
-    if (Math.round(Math.random()) === 0) gameData.last_serve_side = "left";
-    else gameData.last_serve_side = "right";
+    if (Math.round(Math.random()) === 0) gamestate.last_serve_side = "left";
+    else gamestate.last_serve_side = "right";
 
     //Setup initial paddle state
-    gameData.paddle_left.pos.y = 0;
-    gameData.paddle_left.pos.x = -(
+    gamestate.paddle_left.pos.y = 0;
+    gamestate.paddle_left.pos.x = -(
       GameConfig.playAreaWidth / 2 -
       PaddleConfig.borderOffset
     );
-    gameData.paddle_right.pos.y = 0;
-    gameData.paddle_right.pos.x =
+    gamestate.paddle_right.pos.y = 0;
+    gamestate.paddle_right.pos.x =
       GameConfig.playAreaWidth / 2 - PaddleConfig.borderOffset;
 
-    return gameData;
+    return gamestate;
   }
 
   //Create a new game instance
@@ -101,31 +101,31 @@ export class GameLogic {
   }
 
   //Calculate game state and return gamestate object
-  calculateGameState(gameData: GameData): GameData {
+  calculateGameState(gamestate: GameData): GameData {
     //Check if new round
-    if (gameData.is_new_round) {
-      gameData.last_update_time = Date.now();
-      gameData.ball = this.getRandomBallDirection(gameData);
-      gameData.is_new_round = false;
+    if (gamestate.is_new_round) {
+      gamestate.last_update_time = Date.now();
+      gamestate.ball = this.getRandomBallDirection(gamestate);
+      gamestate.is_new_round = false;
     }
     //If not new round, calculate ball position and update timestamp
     else {
-      gameData.ball = this.updateBall(gameData);
-      gameData.last_update_time = Date.now();
+      gamestate.ball = this.updateBall(gamestate);
+      gamestate.last_update_time = Date.now();
     }
-    return gameData;
+    return gamestate;
   }
 
   //Calculate ball position
-  updateBall(gameData: GameData): BallData {
+  updateBall(gamestate: GameData): BallData {
     //Save previous ball data, and create an object for new ball data
-    const prevBall: BallData = gameData.ball;
+    const prevBall: BallData = gamestate.ball;
     const curBall: BallData = new BallData();
 
     curBall.direction = prevBall.direction;
     curBall.speed = prevBall.speed;
     //Get a time difference between last update and this update
-    const time_diff: number = (Date.now() - gameData.last_update_time) / 1000;
+    const time_diff: number = (Date.now() - gamestate.last_update_time) / 1000;
 
     //Find new ball position
     curBall.pos = Vec2.scaleAndAdd(
@@ -133,6 +133,47 @@ export class GameLogic {
       prevBall.direction,
       prevBall.speed * time_diff
     );
+
+    //Check left collision if ball is moving left
+    if (curBall.direction.x < 0) {
+      const intersect: Vec2 = checkIntersect(
+        prevBall.pos,
+        curBall.pos,
+        new Vec2(
+          gamestate.paddle_left.pos.x + PaddleConfig.width / 2,
+          gamestate.paddle_left.pos.y + PaddleConfig.height / 2
+        ),
+        new Vec2(
+          gamestate.paddle_left.pos.x + PaddleConfig.width / 2,
+          gamestate.paddle_left.pos.y - PaddleConfig.height / 2
+        )
+      );
+      if (intersect) {
+        const remainder: Vec2 = Vec2.sub(curBall.pos, intersect); //Find the remaining of the vector that goes past the boundary
+        curBall.pos = Vec2.add(intersect, remainder); //Add the remainder to the intersect point to get the new point
+        curBall.direction.x = -curBall.direction.x; //Invert the direction
+      }
+      //Else check for back wall collision for point score
+      else if (
+        curBall.pos.x <= -(GameConfig.playAreaWidth / 2 + BallConfig.radius)
+      ) {
+        const intersect: Vec2 = checkIntersect(
+          prevBall.pos,
+          curBall.pos,
+          GameConfig.topLeft,
+          GameConfig.botLeft
+        );
+        if (intersect) {
+          const remainder: Vec2 = Vec2.sub(curBall.pos, intersect);
+          curBall.pos = Vec2.add(intersect, remainder);
+          curBall.direction.x = -curBall.direction.x;
+        }
+      }
+    }
+    //Check right collision if ball is moving right
+    // else if (curBall.direction.x > 0) {
+    // }
+    //Then run wall collision, and do score
 
     //Check if new ball position requires collision detection
     if (curBall.pos.x >= GameConfig.playAreaWidth / 2 - BallConfig.radius) {
@@ -146,12 +187,9 @@ export class GameLogic {
       );
       //If return was not null there is an intersection
       if (intersect) {
-        //Find the remaining of the vector that goes past the boundary
-        const remainder: Vec2 = Vec2.sub(curBall.pos, intersect);
-        //Add the remainder to the intersect point to get the new point
-        curBall.pos = Vec2.add(intersect, remainder);
-        //Invert the direction
-        curBall.direction.x = -curBall.direction.x;
+        const remainder: Vec2 = Vec2.sub(curBall.pos, intersect); //Find the remaining of the vector that goes past the boundary
+        curBall.pos = Vec2.add(intersect, remainder); //Add the remainder to the intersect point to get the new point
+        curBall.direction.x = -curBall.direction.x; //Invert the direction
       }
     } else if (
       curBall.pos.x <= -(GameConfig.playAreaWidth / 2 + BallConfig.radius)
@@ -203,7 +241,7 @@ export class GameLogic {
   }
 
   //Get a new random ball direction and velocity
-  getRandomBallDirection(gameData: GameData): BallData {
+  getRandomBallDirection(gamestate: GameData): BallData {
     const ballData: BallData = new BallData();
     ballData.pos = new Vec2(0, 0);
     ballData.speed = BallConfig.initialSpeed;
@@ -225,10 +263,10 @@ export class GameLogic {
     ballData.direction = Vec2.normalize(ballData.direction);
 
     //If last serve was to the right, invert x value to send left
-    if (gameData.last_serve_side === "right") {
+    if (gamestate.last_serve_side === "right") {
       ballData.direction.x = -ballData.direction.x;
-      gameData.last_serve_side = "left";
-    } else gameData.last_serve_side = "right";
+      gamestate.last_serve_side = "left";
+    } else gamestate.last_serve_side = "right";
 
     return ballData;
   }
